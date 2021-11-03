5ea648411f986758208e0a5eccc67201ef136520
YAMCHA <dbals9812@naver.com>
2021-11-04 00:39:07 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+
+      <footer><div>Icons made by <a href="https://www.flaticon.com/authors/vector-clans" title="Vector Clans">Vector Clans</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div></footer>
+    </div>
+  );
+}
+
+export default App;
